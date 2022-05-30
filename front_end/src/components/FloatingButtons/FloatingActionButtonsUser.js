79a6861b5736ecc21b8bf558174696ab2f9f005d
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-05-30 17:20:00 +0200

    add users , add events

--- front_end/src/components/FloatingButtons/FloatingActionButtonsUser.js
+++ front_end/src/components/FloatingButtons/FloatingActionButtonsUser.js
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import AddUser from './AddUser'
+import AddUser from './AddUser';
 import EditProfile from './EditUser';
-import '../../pages/styles.css'
+import '../../pages/styles.css';
 
-
-
-
-export default function FloatingActionButtons() {
+export default function FloatingActionButtons({ reload }) {
   return (
-    <Box sx={{ '& > :not(style)': { m: 1 }, position:'fixed',marginLeft:'80%',marginTop:'-2%',marginRight:'2%'}}>
-      <AddUser/>
-      <EditProfile/>
-      
-        
+    <Box
+      sx={{
+        '& > :not(style)': { m: 1 },
+        position: 'fixed',
+        marginLeft: '80%',
+        marginTop: '-2%',
+        marginRight: '2%',
+      }}
+    >
+      <AddUser reload={reload} />
+      <EditProfile reload={reload} />
     </Box>
   );
 }
